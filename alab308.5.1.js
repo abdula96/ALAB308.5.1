068ec55455ce7802e3cea572d4b817cc45987606
Abdula Ahmed <ahmed.abdula.hslc@gmail.com>
2024-10-31 09:38:16 -0400

    PART 2

--- alab308.5.1.js
+++ alab308.5.1.js
@@ -68,3 +68,39 @@ function countnumbers(n , limit) {
 const output = countnumbers(1, 10).join('\n');
 console.log(output);
 
+/*--------------------Part 2--------------------*/ 
+
+/* provided data */
+const people = [
+    { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+  ];
+  
+  // 1. Sort by age
+  const sortedByAge = people.sort((a, b) => Number(a.age) - Number(b.age));
+  console.log("Sorted by Age:", sortedByAge);
+  
+  // 2. Filter to remove entries with age > 50
+  const filteredByAge = people.filter(person => Number(person.age) <= 50);
+  console.log("Filtered by Age (<= 50):", filteredByAge);
+  
+  // 3. Map to change occupation to job and increment age by 1
+  const mappedPeople = people.map(person => ({
+    id: person.id,
+    name: person.name,
+    job: person.occupation,
+    age: Number(person.age) + 1
+  }));
+  console.log("Mapped Array:", mappedPeople);
+  
+  // 4. Reduce to calculate sum of ages
+  const sumOfAges = people.reduce((sum, person) => sum + Number(person.age), 0);
+  console.log("Sum of Ages:", sumOfAges);
+  
+  // 5. Calculate average age
+  const averageAge = sumOfAges / people.length;
+  console.log("Average Age:", averageAge);
+  
